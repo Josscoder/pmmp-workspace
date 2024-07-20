d3a69b81c6c6092f98c3a3f5612b67ffbeeae5d6
Josscoder <josscoder@hotmail.com>
2024-07-19 20:44:41 -0500

    feat: impl gif & fix class typo

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Here I will upload random things that I do in Pocketmine
 
-- SuperPickaxe: https://youtu.be/APoGcbaD7-8
+## SuperPickaxe
+![](assets/spa.gif)
--- plugins/SuperPickaxe/src/SuperPickaxePlugin.php
+++ plugins/SuperPickaxe/src/SuperPickaxePlugin.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
 use pocketmine\utils\TextFormat;
 use SuperPickaxe\command\SuperPickaxeRadiusCommand;
-use SuperPickaxe\listener\SuperPickAxeListener;
+use SuperPickaxe\listener\SuperPickaxeListener;
 
 class SuperPickaxePlugin extends PluginBase
 {
@@ -22,7 +22,7 @@ class SuperPickaxePlugin extends PluginBase
     protected function onEnable(): void
     {
         $this->getServer()->getCommandMap()->register('superpickaxe', new SuperPickaxeRadiusCommand());
-        $this->getServer()->getPluginManager()->registerEvents(new SuperPickAxeListener(), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new SuperPickaxeListener(), $this);
 
         $this->getLogger()->info(TextFormat::GREEN . 'SuperPickaxe Plugin Enabled');
     }
